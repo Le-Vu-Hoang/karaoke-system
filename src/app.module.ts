374e978bfc7658ae127d3043fcbf18f2a28ab87d
Le Vu Hoang <bx1536hoang@gmail.com>
2026-05-13 15:38:32 +0700

    chore: setup nestjs boilerplate and project configuration

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import { JwtStrategy } from './modules/auth/strategies/jwt.strategy';
+
+@Module({
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+			envFilePath: '.env',
+		}),
+		AuthModule,
+		PrismaModule,
+	],
+	controllers: [AppController],
+	providers: [AppService, JwtStrategy],
+})
+export class AppModule {}
