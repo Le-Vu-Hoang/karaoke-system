39cd29585f7b802ec3e043a1e562157f48e0acb3
Le Vu Hoang <bx1536hoang@gmail.com>
2026-05-13 15:57:37 +0700

    feat:auth service

---
+++ src/common/decorations/get-user.decoration.ts
@@ -0,0 +1,14 @@
+//? Custom decorater to extract user information from the request
+
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { JwtPayload } from '../../modules/auth/interfaces/jwt-payload.interface';
+import { RequestWithUser } from '../../modules/auth/interfaces/req-w-user.interface';
+
+export const GetUser = createParamDecorator(
+	(data: keyof JwtPayload | undefined, ctx: ExecutionContext) => {
+		const request = ctx.switchToHttp().getRequest<RequestWithUser>();
+		const user = request.user;
+
+		return data ? user?.[data] : user;
+	},
+);
---
+++ src/common/guards/refresh-token.guard.ts
@@ -0,0 +1,8 @@
+//? Guard for protecting refresh token endpoint
+import { AuthGuard } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class RefreshTokenGuard extends AuthGuard('jwt-refresh') {
+
+}
